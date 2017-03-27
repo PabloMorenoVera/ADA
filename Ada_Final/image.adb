301190f0b76a6f2b7a2e08233e4fd1e4dc04781c
Pablo Moreno Vera <pablomvera14@gmail.com>
2017-03-27 20:35:13 +0200

    Add files via upload

---
+++ Ada_Final/image.adb
@@ -0,0 +1,105 @@
+--PAblo Moreno Vera
+--Doble Grado Teleco + ADE.
+
+package body Image is
+
+   function Image_3 (T: Ada.Calendar.Time) return String is
+   begin
+      return C_IO.Image(T, "%T.%i");
+   end Image_3;
+
+
+   function Image_2 (T: Ada.Calendar.Time) return String is
+   begin
+      return C_IO.Image(T, "%c");
+   end Image_2;
+
+   procedure Get_IP_Port (Direccion: LLU.End_Point_Type;
+                          IP: out ASU.Unbounded_String;
+                          Port: out ASU.Unbounded_String) is
+
+      IP_Completa: ASU.Unbounded_String;
+      Posicion: Natural;
+
+   begin
+
+      IP_Completa := ASU.To_Unbounded_String(LLU.Image(Direccion));
+      Posicion := ASU.Index (IP_Completa, ":")+1;
+
+      IP_Completa := ASU.Tail (IP_Completa, ASU.Length (IP_Completa) - Posicion) ;
+      IP := (ASU.Head (IP_Completa, ASU.Index (IP_Completa, ",") - 1));
+      Posicion:= ASU.Index (IP_Completa, ":")+2;
+      Port := ASU.Tail (IP_Completa, ASU.Length (IP_Completa) - Posicion) ;
+
+
+   end Get_IP_Port;
+
+   function Mess_Image (Mess: T.Mess_Id_T) return String is
+
+      EP_String: ASU.Unbounded_String;
+      Seq_String: ASU.Unbounded_String;
+      Mess_String: ASU.Unbounded_String;
+      IP: ASU.Unbounded_String;
+      Port: ASU.Unbounded_String;
+
+   begin
+
+      Get_IP_Port(Mess.EP, IP, Port);
+      EP_String := IP & " " & Port;
+      Seq_String := ASU.To_Unbounded_String (T.Seq_N_T'Image(Mess.Seq));
+      Mess_String := EP_String & " " & Seq_String;
+
+      return ASU.To_String (Mess_String);
+
+   end Mess_Image;
+
+   function Destinations_Image (Destination: T.Destinations_T) return String is
+
+      EP_String: ASU.Unbounded_String;
+      Ret_String: ASU.Unbounded_String;
+      Destination_String: ASU.Unbounded_String;
+      IP: ASU.Unbounded_String;
+      Port: ASU.Unbounded_String;
+
+   begin
+
+
+      for i in 1..10 loop
+         if Destination(i).Ep /= Null then
+            Get_IP_Port(Destination(i).EP, IP, Port);
+            EP_String := IP & " " & Port;
+            Ret_String := ASU.To_Unbounded_String(Integer'Image(Destination(i).Retries));
+            Destination_String := EP_String & " " & Ret_String;
+         else
+            Destination_String := ASU.To_Unbounded_String("Null.");
+         end if;
+
+        Destination_String := "|" & Destination_String & "|, ";
+
+      end loop;
+
+
+      return ASU.To_String (Destination_String);
+
+   end Destinations_Image;
+
+   function Value_Image (Value: T.Value_T) return String is
+
+      EPHC_String: ASU.Unbounded_String;
+      Seq_String: ASU.Unbounded_String;
+      Value_String:ASU.Unbounded_String;
+      IP: ASU.Unbounded_String;
+      Port: ASU.Unbounded_String;
+
+   begin
+
+      Get_IP_Port(Value.EP_H_Creat, IP, Port);
+      EPHC_String := IP & " " & Port;
+      Seq_String := ASU.To_Unbounded_String (T.Seq_N_T'Image(Value.Seq_N));
+      Value_String := EPHC_String & " " & Seq_String;
+
+      return ASU.To_String(Value_String);
+
+   end Value_Image;
+
+end Image;
---
+++ Ada_Final/use_timed_handler.ads
@@ -0,0 +1,10 @@
+--Pablo Moreno Vera
+--Doble Grado Teleco + Ade.
+
+with Ada.Calendar;
+
+package Use_Timed_Handler is
+
+   procedure Retransmitir_Mensaje (Time: Ada.Calendar.Time);
+
+end Use_Timed_Handler;
